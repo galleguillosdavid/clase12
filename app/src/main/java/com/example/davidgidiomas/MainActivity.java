e01c65e26bf27327cdf35f4a7d5e136225fba5ff
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-11 21:42:16 -0400

    V 2.0

--- app/src/main/java/com/example/davidgidiomas/MainActivity.java
+++ app/src/main/java/com/example/davidgidiomas/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.davidgidiomas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.prathameshmore.toastylibrary.Toasty;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView =findViewById(R.id.txt1);
+
+
+        if(BuildConfig.DEBUG){
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        final Toasty toasty = new  Toasty(MainActivity.this);
+        toasty.primaryToasty(this, "La tercera es la Vencida", Toasty.LENGTH_LONG, Toasty.BOTTOM);
     }
+   /* protected  void onStart(){
+        super.onStar();
+        Timber.d("OnStart");
+    }*/
 }
