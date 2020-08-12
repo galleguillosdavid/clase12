cd675b57dfe9f98f0e1e9613584d416d79aaf29c
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-11 22:02:45 -0400

    V 3.0

--- app/src/main/java/com/example/davidgidiomas/MainActivity.java
+++ app/src/main/java/com/example/davidgidiomas/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.davidgidiomas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.prathameshmore.toastylibrary.Toasty;
@@ -10,20 +12,28 @@ import com.prathameshmore.toastylibrary.Toasty;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
-
+    final Toasty toasty = new  Toasty(MainActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView =findViewById(R.id.txt1);
 
+        Button boton = findViewById(R.id.boton1);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+
+                toasty.primaryToasty(view.getContext(), "La tercera es la Vencida", Toasty.LENGTH_LONG, Toasty.BOTTOM);
+            }
+        });
         if(BuildConfig.DEBUG){
             Timber.plant(new Timber.DebugTree());
         }
 
-        final Toasty toasty = new  Toasty(MainActivity.this);
-        toasty.primaryToasty(this, "La tercera es la Vencida", Toasty.LENGTH_LONG, Toasty.BOTTOM);
+
+
     }
    /* protected  void onStart(){
         super.onStar();
